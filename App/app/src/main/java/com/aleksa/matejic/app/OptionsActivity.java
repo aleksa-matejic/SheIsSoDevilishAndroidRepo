5846aedf86f54155f68a09d7b0064668000a9483
Aleksa Matejic <aleksa.matejic@endava.net>
2017-02-26 16:31:23 +0100

    Adding code to repository

---
+++ App/app/src/main/java/com/aleksa/matejic/app/OptionsActivity.java
@@ -0,0 +1,15 @@
+package com.aleksa.matejic.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
